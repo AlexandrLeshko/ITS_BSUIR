2f240ab79bf4a7e7a8e0b63b394c8e9483108c6b
a.leshko <a.leshko@bsuir.by>
2023-04-12 11:45:20 +0300

    fix: <header> small bugs + visual (wed 12.04.2023 11:45)

--- burger__menu.js
+++ burger__menu.js
@@ -2,6 +2,12 @@ const burger__button = document.querySelector('.burger__button');
 const burger__menu = document.querySelector('.burger__menu');
 
 burger__button.addEventListener('click', () => {
-	burger__button.classList.toggle('burger--clicked');
 	burger__menu.classList.toggle('burger--opened');
+	if (burger__menu.classList.contains('burger--opened')) {
+		burger__button.innerHTML = '✕';
+		burger__button.style.transform = 'rotateZ(-90deg)';
+	} else {
+		burger__button.innerHTML = '☰';
+		burger__button.style.transform = 'rotateZ(0deg)';
+	}
 });
