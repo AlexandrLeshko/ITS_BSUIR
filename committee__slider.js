cf702009136c230384be9124fe055e5fe22de13c
a.leshko <a.leshko@bsuir.by>
2023-04-19 14:53:21 +0300

    feat: committee slider (wed 19.04.2023 14:53)

---
+++ committee__slider.js
@@ -0,0 +1,21 @@
+const committee__btnOne = document.querySelector('.committee__button:nth-child(1)');
+const committee__btnTwo = document.querySelector('.committee__button:nth-child(2)');
+const committee__info = document.querySelector('.committee__info')
+const committee__mover = document.querySelector('.committee__topic:nth-child(1)');
+
+committee__mover.style.marginLeft = '0px';
+committee__info.style.borderRadius = '0 10px 10px 10px';
+
+committee__btnOne.addEventListener('click', () => {
+	committee__btnOne.classList.add('committee__button--selected');
+	committee__btnTwo.classList.remove('committee__button--selected');
+	committee__mover.style.marginLeft = '0px';
+	committee__info.style.borderRadius = '0 10px 10px 10px';
+});
+
+committee__btnTwo.addEventListener('click', () => {
+	committee__btnOne.classList.remove('committee__button--selected');
+	committee__btnTwo.classList.add('committee__button--selected');
+	committee__mover.style.marginLeft =  - committee__mover.clientWidth + 'px';
+	committee__info.style.borderRadius = '10px 10px 10px 10px';
+});
