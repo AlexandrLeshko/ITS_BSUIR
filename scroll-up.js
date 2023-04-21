f4dbb92815a09bfb71411494c79166a33d537eb8
a.leshko <a.leshko@bsuir.by>
2023-04-21 14:32:20 +0300

    feat: add scroll-up button (fri 21.04.2023 14:32)

---
+++ scroll-up.js
@@ -0,0 +1,29 @@
+const scrollUp = document.querySelector('.scroll-up');
+const header = document.querySelector('header');
+
+function Visible(target) {
+	let targetPosition = {
+		top: window.pageYOffset + target.getBoundingClientRect().top,
+		left: window.pageXOffset + target.getBoundingClientRect().left,
+		right: window.pageXOffset + target.getBoundingClientRect().right,
+		bottom: window.pageYOffset + target.getBoundingClientRect().bottom
+	};
+	let windowPosition = {
+		top: window.pageYOffset,
+		left: window.pageXOffset,
+		right: window.pageXOffset + document.documentElement.clientWidth,
+		bottom: window.pageYOffset + document.documentElement.clientHeight
+	};
+	if (targetPosition.bottom > windowPosition.top && targetPosition.top < windowPosition.bottom && targetPosition.right > windowPosition.left && targetPosition.left < windowPosition.right) {
+		scrollUp.style.display = 'none';
+		scrollUp.style.opacity = '0';
+	} else {
+		scrollUp.style.display = 'block';
+		scrollUp.style.opacity = '0.8';
+	};
+};
+Visible (header);
+
+window.addEventListener('scroll', function() {
+	Visible (header);
+});
